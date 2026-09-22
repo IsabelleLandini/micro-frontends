8411d859fd0ab73c40549730962d0b3d4f47ec9f
Isabelle <isa_landini@hotmail.com>
2026-09-22 16:07:56 -0300

    feat: configura module federation no cardapio

--- cardapio/next.config.mjs
+++ cardapio/next.config.mjs
@@ -1,6 +1,22 @@
+import { NextFederationPlugin } from '@module-federation/nextjs-mf';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  webpack: (config, options) => {
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: "cardapio",
+        filename: "static/chunks/remoteEntry.js",
+
+        exposes: {
+          "./Prato": "./components/Prato.js",
+        },
+      })
+    )
+    return config;
+  },
 };
 
 export default nextConfig;
