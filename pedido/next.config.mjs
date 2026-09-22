5baac245571a6d57ae41b06cf93b42a24725e245
Isabelle <isa_landini@hotmail.com>
2026-09-22 17:10:38 -0300

    feat: configura module federation no pedido

--- pedido/next.config.mjs
+++ pedido/next.config.mjs
@@ -1,6 +1,22 @@
+import { NextFederationPlugin } from "@module-federation/nextjs-mf";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  webpack: (config, options) => {
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: "pedido",
+        filename: "static/chunks/remoteEntry.js",
+
+        exposes: {
+          "./Pedido": "./pages/index.js",
+        },
+      })
+    );
+    return config;
+  },
 };
 
 export default nextConfig;
